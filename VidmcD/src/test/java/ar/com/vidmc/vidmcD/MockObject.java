e5726a528370a44239bd614d6f57ebb7ed890ffa
Matias Tarrio Pages <matias.tarriopages@gmail.com>
2012-03-09 17:48:11 -0300

    RepositoryTest fixed

---
+++ VidmcD/src/test/java/ar/com/vidmc/vidmcD/MockObject.java
@@ -0,0 +1,15 @@
+package ar.com.vidmc.vidmcD;
+
+public class MockObject {
+	
+	private String name;
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+
+}
--- VidmcD/src/test/java/ar/com/vidmc/vidmcD/RepositoryTest.java
+++ VidmcD/src/test/java/ar/com/vidmc/vidmcD/RepositoryTest.java
@@ -2,7 +2,9 @@ package ar.com.vidmc.vidmcD;
 
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import ar.com.vidmc.repository.DataBase;
@@ -10,14 +12,26 @@ import ar.com.vidmc.repository.Repository;
 
 public class RepositoryTest {
 
+	@Before
+	public void Setup(){
+		DataBase.getInstance().startDataBase();
+	}
+	
+	@After
+	public void TearDown(){
+		DataBase.getInstance().stopDataBase();
+	}
+	
 	@Test
 	public void ConectionTest(){
-		DataBase.getInstance().startDataBase();
-		Repository.getInstancia().store(new Object());
-		List<Object> set = Repository.getInstancia().queryByExample(new Object());
-		Assert.assertTrue(set.size()>=0);
-		for(Object o:set){
+		
+		Repository.getInstancia().store(new MockObject());
+		List<MockObject> set = Repository.getInstancia().queryByExample(new MockObject());
+		Assert.assertTrue(set.size()==1);
+		for(MockObject o:set){
 			Repository.getInstancia().delete(o);
 		}
+		set = Repository.getInstancia().queryByExample(new MockObject());
+		Assert.assertTrue(set.size()==0);
 	}
 }
